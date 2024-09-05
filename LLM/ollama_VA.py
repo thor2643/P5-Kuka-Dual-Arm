6cd2b0baec8a6f1a1801632e1b31434cdf22bc44
thor2643 <Thor951753@hotmail.com>
2024-09-05 11:23:06 +0200

    Added LLM examples

---
+++ LLM/ollama_VA.py
@@ -0,0 +1,37 @@
+
+import requests
+import json
+
+# Define the URL of OLLAMA (VLM and LLM)
+url = "http://localhost:11434/api/generate"
+
+# Define the LLM models
+llm_model = ['phi3:3.8b']
+
+while True:
+
+    prompt = input("user: ")
+
+    # setup the payload for http request of llm
+    payload = {
+        "model": llm_model[0],
+        "prompt": prompt,
+        "stream": False,
+    }
+
+    # Perform the POST request with data
+    response = requests.post(url, headers={"Content-Type": "application/json"}, data=json.dumps(payload))
+    print(response)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        try:
+            # Attempt to parse the JSON response
+            response = response.json()
+            print("VA: ", response['response'])
+        except requests.exceptions.JSONDecodeError as e:
+            print("JSON Decode Error:", e)
+            continue
+    else:
+        print("Error:", response.status_code, response.text)
+        continue
