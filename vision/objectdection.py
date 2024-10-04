85245ed4fb576f60b48d8f751844baccaf3a4100
DaMalle <marcfriis@student.aau.dk>
2024-10-04 14:09:42 +0200

    made detection-class return a dict with detection info

--- vision/objectdection.py
+++ vision/objectdection.py
@@ -4,21 +4,29 @@ import numpy as np
 import cv2
 
 
-class RealSense:
+class RealSenseCamera:
     def __init__(self):
         # Configure depth and color streams
         self.pipeline = rs.pipeline()
         self.config = rs.config()
 
-        self.config.enable_stream(
-            rs.stream.depth, 640, 480, rs.format.z16, 30
-        )
+        # self.config.enable_stream(
+        #     rs.stream.depth, 640, 480, rs.format.z16, 30
+        # )
         self.config.enable_stream(
             rs.stream.color, 640, 480, rs.format.bgr8, 30
         )
 
     def __enter__(self):
         self.pipeline.start(self.config)
+
+        color_sensor = self.pipeline.get_active_profile().get_device().query_sensors()[1]
+
+        # set options for color sensor
+        color_sensor.set_option(rs.option.brightness, 0)
+        color_sensor.set_option(rs.option.exposure, 166)
+        color_sensor.set_option(rs.option.white_balance, 4200)
+
         return self.pipeline
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -28,39 +36,34 @@ class RealSense:
 class ObjectDetector:
     def __init__(self):
         self.model = YOLOWorld("yolov8x-world.pt")
-        # results = self.model.predict("test.jpg")
-        self.realsense_camera = RealSense
-
-    def detect_all(self) -> None:
-        with self.realsense_camera() as cam:
-            is_q_key_pressed = (cv2.waitKey(1) & 0xFF == ord('q'))
-            while not is_q_key_pressed:
-                frames = cam.wait_for_frames()
-                color_frame = frames.get_color_frame()
-                color_image = np.asanyarray(color_frame.get_data())
-                # results = self.model(color_image)
-                # print(results)
-                cv2.imshow("color", color_image)
+        self.realsense_camera = RealSenseCamera
+        self.detection_groups = dict()
 
     def detect_frame(self) -> None:
         with self.realsense_camera() as cam:
             frames = cam.wait_for_frames()
             color_frame = frames.get_color_frame()
-            color_image = np.asanyarray(color_frame.get_data())
-            results = self.model(color_image, stream=True)
-            print(results)
+            color_frame = np.asanyarray(color_frame.get_data())
 
-            is_q_key_pressed = (cv2.waitKey(1) & 0xFF == ord('q'))
-            while not is_q_key_pressed:
-                cv2.imshow("color", color_image)
+            results = self.model(color_frame, verbose=False, stream=False)[0]
 
-        cv2.destroyAllWindows()
-        # results[0].show()
+            for detection in results.boxes:
+                x, y, *_ = detection.xywh[0]
+                idx = int(detection.cls[0])
+                key = results.names[idx]
+
+                self._insert_into_detection_groups(key=key, info=(x, y))
+
+            return self.detection_groups
+
+    def _insert_into_detection_groups(self, key: str, info: tuple[int, int]) -> None:
+            if key in self.detection_groups.keys():
+                self.detection_groups[key].append(info)
+            else:
+                self.detection_groups[key] = [info]
+
 
 
 if __name__ == "__main__":
-    # cam = RealSense()
-    # cam.get_image()
-    # cam.get_image("test.jpg")
     obj = ObjectDetector()
-    obj.detect_frame()
+    print(obj.detect_frame())
